5279d0ce98a27f6454773131c92998e75a3e5668
Mehran Rasheed <engr.mehranrasheed@gmail.com>
2023-09-17 13:45:57 +0500

    Finished routing and navigation.

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { Suspense } from "react";
 import UserTable from "./UserTable";
+import Link from "next/link";
 
 interface Props {
   searchParams: { sortOrder: string };
 }
 
 const UsersPage = async ({ searchParams: { sortOrder } }: Props) => {
-  console.log(sortOrder);
   return (
     <>
       <h1>Users</h1>
-      <UserTable sortOrder={sortOrder} />
+      <Link href="/users/new" className="btn">
+        NEW USER
+      </Link>
+      <Suspense fallback={<p>Loading...</p>}>
+        <UserTable sortOrder={sortOrder} />
+      </Suspense>
     </>
   );
 };
